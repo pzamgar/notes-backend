e7aa0db1249c0318cc858ae66b852b4429d4a8de
Pere Zamora <pzamgar@gmail.com>
2019-07-09 08:52:35 +0200

    Add files static frontend app

---
+++ build/precache-manifest.c563c14deb98b1b20371ffc8e57f22c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b83641bbd32a52519c494f601e4eb5af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31e0117a70de626cd3a3",
+    "url": "/static/css/main.1716783e.chunk.css"
+  },
+  {
+    "revision": "c80183385d0c3c56b9c2",
+    "url": "/static/js/2.66f27ac9.chunk.js"
+  },
+  {
+    "revision": "31e0117a70de626cd3a3",
+    "url": "/static/js/main.d60f2ebd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
